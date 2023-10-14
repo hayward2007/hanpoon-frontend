d5b5b3e498e71b675c3ac46868b486e900c10078
hayward2007 <hayward2007@naver.com>
2023-10-14 20:37:50 +0900

    Base Structed

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import Home from './Home';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Analysis from './Analysis';
+import Search from './Search';
+import All from './All';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {ScrollView, Text} from 'react-native';
+import NavigationBar from '../components/navigation';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          animation: 'fade',
+          animationDuration: 200,
+        }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Analysis" component={Analysis} />
+        <Stack.Screen name="Search" component={Search} />
+        <Stack.Screen name="All" component={All} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
